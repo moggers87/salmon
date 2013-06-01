5e3885571ad7d93c03a3b23fa61d31ea4bd68ab0
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2013-06-01 02:30:21 +0100

    Sort out setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,20 @@ except ImportError:
 config = {
     'package_data': {
         'salmon': ['data/prototype.zip']
-    }, 
-    'description': 'Lamson is a modern Pythonic mail server built like a web application server.',
+    },
+    'description': 'A Python mail server'
+    'long_description': 'Salmon is a modern Pythonic mail server built like a web application server.',
     'author': 'Zed A. Shaw',
     #'url': 'http://pypi.python.org/pypi/salmon',
     #'download_url': 'http://pypi.python.org/pypi/salmon',
-    'author_email': 'zedshaw@zedshaw.com',
-     'version': '1.2',
-     'scripts': ['bin/salmon'],
-     'install_requires': ['chardet', 'jinja2', 'mock', 'nose', 'python-daemon', 'python-modargs'],
-     'packages': ['salmon',
-     'salmon.handlers'],
-     'name': 'salmon'
+    'maintainer': 'Matt Molyneaux',
+    'maintainer_email': 'moggers87+git@moggers87.co.uk',
+    'version': '0',
+    'scripts': ['bin/salmon'],
+    'install_requires': ['chardet', 'jinja2', 'mock', 'nose', 'python-daemon', 'python-modargs'],
+    'packages': ['salmon', 'salmon.handlers'],
+    'name': 'salmon',
+    'license': 'GPLv3'
 }
 
 setup(**config)
