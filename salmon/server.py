a479a641c35963ff52b6fb198f082c23a2e04073
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-12-24 15:37:33 +0000

    QueueReceiver wasn't expecting new mail class
    touch #21

--- salmon/server.py
+++ salmon/server.py
@@ -310,17 +310,13 @@ class QueueReceiver(object):
         """
 
         try:
-            Peer = self.queue_dir # this is probably harmless but I should check it
-            From = msg['from']
-            To = [msg['to']]
-
-            logging.debug("Message received from Peer: %r, From: %r, to To %r.", Peer, From, To)
+            logging.debug("Message received from Peer: %r, From: %r, to To %r.", msg.Peer, msg.From, msg.To)
             routing.Router.deliver(msg)
         except SMTPError, err:
             # looks like they want to return an error, so send it out
             logging.exception("Raising SMTPError when running in a QueueReceiver is unsupported.")
-            undeliverable_message(msg.original, err.message)
+            undeliverable_message(msg.Data, err.message)
         except Exception:
             logging.exception("Exception while processing message from Peer: "
-                              "%r, From: %r, to To %r.", Peer, From, To)
-            undeliverable_message(msg.original, "Router failed to catch exception.")
+                              "%r, From: %r, to To %r.", msg.Peer, msg.From, msg.To)
+            undeliverable_message(msg.Data, "Router failed to catch exception.")
