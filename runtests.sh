728dd17615cc9b266e48d6f27edd70e7de1a73fa
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2015-07-19 20:08:15 +0100

    Update runtest.sh script with new commandline options
    Touch #28

--- runtests.sh
+++ runtests.sh
@@ -2,14 +2,14 @@
 
 # start logging server
 mkdir logs run
-./bin/salmon log -pid "./log.pid" -port 8899
+python -c "from salmon import commands; commands.main()" log --pid "./log.pid" --port 8899
 
 # run tests
 python setup.py nosetests
 return_code=$?
 
 # stop logging server
-./bin/salmon stop -pid "./log.pid"
+python -c "from salmon import commands; commands.main()" stop --pid "./log.pid"
 if [ $return_code == 0 ]; then
     rm -r logs run
 fi
